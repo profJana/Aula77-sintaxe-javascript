fee12db6fe07f977050ac011be87f51f2b274ffc
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-03-29 23:37:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+console.log("Oi! Eu sou uma tela de console");
+
+var primeiroNome = "Janaina";
+console.log(primeiroNome);
+
+var ultimoNome = "Tavares";
+console.log(ultimoNome);
+
+var nomeCompleto = primeiroNome.concat(ultimoNome);
+console.log(nomeCompleto);
+
+var comprimentoNome = nomeCompleto.length;
+console.log(comprimentoNome);
+
+var nome1 = "Oi, eu sou a Prof Jana";
+console.log(nome1);
+
+var trocarNome = nome1.replace("Jana", "Jane");
+console.log(trocarNome);
+
+var nomeMaiusculo = "JANA";
+var convertendoEmMinusculo = nomeMaiusculo.toLowerCase();
+console.log(convertendoEmMinusculo);
+
+var nomeMinusculo = "jana";
+var convertendoEmMaiusculo = nomeMinusculo.toUpperCase();
+console.log(convertendoEmMaiusculo);
+
+var raizQuadrada = Math.sqrt(16);
+console.log(raizQuadrada);
+
+var numMaximo = Math.max(15, 10);
+console.log(numMaximo);
+
+var numMinimo = Math.min(15, 10);
+console.log(numMinimo);
+
+console.log("Atividade Adicional");
+
+var numeroGrande = 3.141592653589793;
+console.log(numeroGrande);
+
+var toFixed = numeroGrande.toFixed(2);
+console.log(toFixed);
+
+var num1 = 12;
+var num2 = 3;
+console.log(num1 + num2);
+
+var num1toString = num1.toString();
+var num2toString = num2.toString();
+console.log(num1toString + num2toString);
+
+var tipoVariavel = 6;
+console.log(typeof tipoVariavel);
+
+var tipoVariavel2 = "6";
+console.log(typeof tipoVariavel2);
+
+var incremento = 10;
+incremento++;
+console.log(incremento);
+
+var decremento = 10;
+decremento--;
+console.log(decremento);
